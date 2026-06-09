83d217491c543fb4b476abd6f002b10e3ea82ff8
Shivam Raikwar <shivamrkr93@gmail.com>
2026-06-09 12:29:28 +0530

    Fix middleware order

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,15 @@
-
-const cors = require("cors"); // frontend connection
-const express = require("express")
+const cors = require("cors");
+const express = require("express");
 const contactRoutes = require("./routes/contact.routes");
 const cvRoutes = require("./routes/cv.routes");
- 
 const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use("/api/cv", cvRoutes);
-app.use(cors()) // frontend connection
+app.use(cors());
 app.use(express.json());
 
+app.use("/api/cv", cvRoutes);
 app.use("/api/contact", contactRoutes);
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
@@ -20,4 +18,4 @@ app.get("/", (req, res) => {
     res.send("Server running");
 });
 
-module.exports =  app;
+module.exports = app;
